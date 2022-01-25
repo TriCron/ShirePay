5c999fbbaa3b4f3aabdf0d6de1f3c2bff4c22955
Darren Conroy <TriCron@users.noreply.github.com>
2022-01-25 11:10:32 +0000

    ShirePay Xamarin Forms Init

---
+++ ShirePayApp/ShirePayApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ShirePayApp.Services;
+using ShirePayApp.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShirePayApp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ShirePayApp/ShirePayApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ShirePayApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ShirePayApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
